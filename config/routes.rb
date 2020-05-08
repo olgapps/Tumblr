4964c335ca3a25baae132bacbdd942282b2c5ea0
olga <olg262@gmail.com>
2020-05-08 15:58:26 +0200

    about page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do 
+    resource :comments
+  end
   root "posts#index"
+
+  get '/about', to: 'pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
